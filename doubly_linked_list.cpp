ce2416ee3cb0e4e9d9695158a16b23670808812f
Arsalan099 <khan038299@gmail.com>
2018-09-30 11:16:26 +0530

    Information added and XOR linked list description

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,3 +1,38 @@
+/*
+
+It has both previous and next pointer
+Advantages
+1. Can traverse in both forward and backward direction.
+2. Delete operation becomes easy.
+3. Insertion at any point also becomes easy as we know the previous and next node address.
+
+Disadvantges
+1. Extra space needed for previous pointer.
+2. Extra operation required for maintainance of the operation.
+
+
+XOR APPROACH
+What we do here is we create a XOR of next and previous nodes and store it in npx
+
+Node A:
+npx = 0 XOR add(B) // bitwise XOR of zero and address of B
+
+Node B:
+npx = add(A) XOR add(C) // bitwise XOR of address of A and address of C
+
+Node C:
+npx = add(B) XOR add(D) // bitwise XOR of address of B and address of D
+
+Node D:
+npx = add(C) XOR 0 // bitwise XOR of address of C and 0
+
+While traversiong we need the address of previous node in order to calculate the next node.
+For example when we are at node C, we must have address of B. XOR of add(B) and npx of C gives us the add(D).
+The reason is simple: npx(C) is “add(B) XOR add(D)”.
+If we do xor of npx(C) with add(B), we get the result as “add(B) XOR add(D) XOR add(B)” which is “add(D) XOR 0” which is “add(D)”.
+So we have the address of next node. Similarly we can traverse the list in backward direction. 
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
